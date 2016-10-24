8a34e697a95680bcd13eabe98b45498b03bf370c
VlastaKoudelka <vlasta.koudelka@gmail.com>
2016-10-24 14:52:45 +0200

    appropriate t-SNE mapping, no significant structure in data

--- Tsne_Micro_states.py
+++ Tsne_Micro_states.py
@@ -9,14 +9,19 @@ import tsne
 import matplotlib.pyplot as plt
 import numpy as np
 
-perplexity = 20
+perplexity = 50
 initdims = 19
 outdims = 2
 
-file_path = '/home/vlastimilo/NUDZ_Data/Microstates/'
+#file_path = '/home/vlastimilo/NUDZ_Data/Microstates/'
+
+file_path = 'D:\MS_data\\'
 file_name = 'BAP-Brisova-avg-19ele-GFPpeaks.txt'
 
 GFP_data = np.loadtxt(file_path+file_name)
+#GFP_data.astype(int)
+GFP_data = GFP_data/np.amax(GFP_data)
+#GFP_data = GFP_data[450:500]
 
 [mapped,C] = tsne.tsne(GFP_data, outdims, initdims, perplexity)
 
