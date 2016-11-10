842c152a4e2e79ee770a72c354d3a7fa030546cf
Vlastimilo <vlastimil.koudelka@nudz.cz>
2016-11-10 09:52:18 +0100

    Informed clustering update

--- Informed_MS.py
+++ Informed_MS.py
@@ -20,29 +20,29 @@ crit_kmeans = np.zeros((3,9))
 for i,n_clst in enumerate(np.arange(2,11,1)):
     obj_kmeans = cluster.KMeans(n_clst)
     labels_kmeans = obj_kmeans.fit_predict(GFP_data)
-    
+    print i
     obj_aglo = cluster.AgglomerativeClustering(n_clst)
     labels_aglo = obj_aglo.fit_predict(GFP_data)
     
-#    obj_spect = cluster.SpectralClustering(n_clst)
-#    labels_spect = obj_spect.fit_predict(GFP_data)
+    obj_spect = cluster.SpectralClustering(n_clst)
+    labels_spect = obj_spect.fit_predict(GFP_data)
     
     crit_kmeans[0,i] = metrics.silhouette_score(GFP_data,labels_kmeans)
     crit_kmeans[1,i] = metrics.silhouette_score(GFP_data,labels_aglo)
-#    crit_kmeans[2,i] = metrics.silhouette_score(GFP_data,labels_aglo)
+    crit_kmeans[2,i] = metrics.silhouette_score(GFP_data,labels_aglo)
 
 plt.figure
 x =np.arange(2,11,1)
 plt.plot(x,crit_kmeans[0],label='Kmeans')
 plt.plot(x,crit_kmeans[1],label='Agglomerative')
-#plt.plot(x,crit_kmeans[2])
-plt.title('KMeans clustering indices')
+plt.plot(x,crit_kmeans[2],label='Spectral')
+plt.title('Informed algorithm clustering')
 plt.xlabel('number of clusters')
 plt.ylabel('silhouette criterion')
 plt.legend()
 plt.show    
 
-f_name = 'kmeans_indices.jpeg'
+f_name = 'Informed_algorithms.jpeg'
 plt.savefig(f_name, dpi=300, facecolor='w', edgecolor='w',
 orientation='portrait', papertype=None, format=None,
 transparent=False, bbox_inches=None, pad_inches=0.1,
