f46167a23d524294fabb936fde75337853915ef8
VlastaKoudelka <vlasta.koudelka@gmail.com>
2017-01-03 16:40:21 +0100

    spatial and temporal approaches implemented

--- MS_varimax.m
+++ MS_varimax.m
@@ -13,43 +13,67 @@ Nt = size(M,1);
 H = eye(Ns) - ones(Ns)/Ns;  %linear average reference transformation mat.
 M = (M * H)';               %average reference and transpose
 M_stand = zscore(M,0,1);    %standardize all topomaps
-M_pca = pca(M_stand);       %initial factors from PCA
-MS_pca = M_stand * M_pca(:,1:4); %project original data into PCA space
-[M_varimax,T] = rotatefactors(M_pca(:,1:4)); %rotate the first four (varimax is default)
-MS_varmax = M_stand * M_varimax;                  %project original data into rotated space
+M_stand = M;
 
+%% Relaxed clusturing approach
+M_pca_components = pca(M_stand);       %initial factors from PCA
+MS_pca = M_stand * M_pca_components(:,1:4); %project original data into PCA space
+[M_varimax_components,T] = rotatefactors(M_pca_components(:,1:4)); %rotate the first four (varimax is default)
+MS_varmax = M_stand * M_varimax_components;             %project original data into rotated space
+
+
+%% PM approach
+PM_pca_components = pca(M_stand');
+MS_PM_pca = PM_pca_components(:,1:4);   %here the components are the MS
+[MS_PM_varimax,T] = rotatefactors(PM_pca_components(:,1:4));
+
+%% Visualize
 figure
-suptitle('Varimax Microstates') %visualize topomaps
+suptitle('Varimax Microstates-temporal') %visualize topomaps
 for i = 1:size(MS_varmax,2)
     subplot(2,2,i)
     topoplot(MS_varmax(:,i),'10-20-microst.loc','gridscale',150);
 end
 
 figure
-suptitle('PCA Microstates') %visualize topomaps
+suptitle('PCA Microstates-temporal') %visualize topomaps
 for i = 1:size(MS_varmax,2)
     subplot(2,2,i)
     topoplot(MS_pca(:,i),'10-20-microst.loc','gridscale',150);
 end
 
+figure
+suptitle('Varimax Microstates - spatial') %visualize topomaps
+for i = 1:size(MS_PM_varimax,2)
+    subplot(2,2,i)
+    topoplot(MS_PM_varimax(:,i),'10-20-microst.loc','gridscale',150);
+end
+
+figure
+suptitle('PCA Microstates-spatial') %visualize topomaps
+for i = 1:size(MS_PM_pca,2)
+    subplot(2,2,i)
+    topoplot(MS_PM_pca(:,i),'10-20-microst.loc','gridscale',150);
+end
+
 %% Mapping the relaxed solution to MS constrains
 
-M_pca = M_pca(:,1:4);   %pick the first four components
+M_pca_components = M_pca_components(:,1:4);   %pick the first four components
 
-for i = 1:size(M_pca,1)
-    vec = M_pca(i,:);
+for i = 1:size(M_pca_components,1)
+    vec = M_pca_components(i,:);
     vec(abs(vec)<max(abs(vec))) = 0;
-    M_pca(i,:) = vec;
+    M_pca_components(i,:) = vec;
     
-    vec = M_varimax(i,:);
+    vec = M_varimax_components(i,:);
     vec(abs(vec)<max(abs(vec))) = 0;
-    M_varimax(i,:) = vec;
+    M_varimax_components(i,:) = vec;
 end
 
 %% Explained variance
 M_stand = M_stand';
-aprox_varimax = M_varimax * MS_varmax';
-aprox_pca = M_pca * MS_pca';
+aprox_varimax = M_varimax_components * MS_varmax';
+aprox_pca = M_pca_components * MS_pca';
 
 var_varimax = trace((M_stand - aprox_varimax)*(M_stand - aprox_varimax)')/(Nt*(Ns-1))
 var_pca = trace((M_stand - aprox_pca)*(M_stand - aprox_pca)')/(Nt*(Ns-1))
@@ -61,8 +85,8 @@ explained_pca = 1 - var_pca/var_data
 
 %% Additional visualization
 
-M_pca_sq = M_pca.^2;   
-M_varimax_sq = M_varimax.^2;
+M_pca_sq = M_pca_components.^2;   
+M_varimax_sq = M_varimax_components.^2;
 
 
 
