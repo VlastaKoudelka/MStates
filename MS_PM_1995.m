bcadfced65a5d8084c40319a01c3cf45833c6fb4
VlastaKoudelka <vlasta.koudelka@gmail.com>
2017-01-06 10:10:06 +0100

    Pascual Marqui algorithm implemented

---
+++ MS_PM_1995.m
@@ -0,0 +1,77 @@
+%% P.M. 1995 algorithm
+
+close all
+clear all
+path = 'D:\MicroStates\MS_data\';
+filename = 'BAP-Brisova-avg-19ele-GFPpeaks.txt';
+
+M = dlmread([path,filename],' ');
+
+Ns = 19;
+Nu = 4;
+Nt = size(M,1);
+sig0 = 0;
+sigu = 1; %an initial value of residual MS model variance
+eps = 1e-6;
+M = M(:,1:Ns);
+Nt = size(M,1);
+H = eye(Ns) - ones(Ns)/Ns;  %linear average reference transformation mat.
+M = (M * H);               %average reference and transpose
+
+%Generate initial labels step 2b) in P.M. 1995
+labels = (rand(Nt,Nu));
+for t = 1:Nt
+    vec = labels(t,:);
+    vec(abs(vec)<max(abs(vec)))=0;
+    labels(t,:)=ceil(vec);
+end
+i = 1;
+while abs(sig0 - sigu) > eps*sigu %step 6)
+    sig0 = sigu;
+    
+    %step 4)
+    for k = 1:Nu        %over all MicroStates
+        S{k} = zeros(Ns);
+        for t = 1:Nt    %over all time samples
+            if labels(t,k) == 1     %accumulate only selected samples
+                S{k} = S{k} + M(t,:)'*M(t,:);
+            end
+        end
+        [V,D]=eig(S{k});    %derive eigenvectors
+        MS(:,k)=V(:,end);   %the last column eigenvect. has the largest
+    end 
+   
+    %step 5)
+    MS_aprox = labels*MS';
+    sigu = trace(M*M' - (MS_aprox*M').^2)/(Nt*(Ns-1));   
+    conv(i) = sigu;
+    %step 3)
+    dist = (M*MS).^2;
+    for t = 1:Nt
+        vec = dist(t,:);
+        vec(vec<max(vec))=0;
+        vec(vec~=0)=1;
+        labels(t,:)=vec;        
+    end
+    i = i+1;
+end
+%% Explained variance
+
+%variance of data
+var_data = trace(M*M')/(Nt*(Ns-1));
+expl = (1 - conv/var_data)*100;
+%% Visualize
+figure(1)
+suptitle('P.M. Microstates') %visualize topomaps
+for i = 1:size(MS,2)
+    subplot(2,2,i)
+    topoplot(MS(:,i),'10-20-microst.loc','gridscale',150);
+end
+
+figure(2)
+plot(expl)
+title('Explained variance over iteration')
+xlabel('Iteration')
+ylabel('Eplained variance [%]')
+
+    
--- MS_varimax.m
+++ MS_varimax.m
@@ -12,24 +12,70 @@ M = M(:,1:Ns);
 Nt = size(M,1);
 H = eye(Ns) - ones(Ns)/Ns;  %linear average reference transformation mat.
 M = (M * H)';               %average reference and transpose
-M_stand = zscore(M,0,1);    %standardize all topomaps
-M_stand = M;
 
-%% Relaxed clusturing approach
-M_pca_components = pca(M_stand);       %initial factors from PCA
-MS_pca = M_stand * M_pca_components(:,1:4); %project original data into PCA space
+%% Relaxed clusturing approach - spatial
+M_pca_components = pca(M);       %initial factors from PCA
+MS_pca = M * M_pca_components(:,1:4); %project original data into PCA space
 [M_varimax_components,T] = rotatefactors(M_pca_components(:,1:4)); %rotate the first four (varimax is default)
-MS_varmax = M_stand * M_varimax_components;             %project original data into rotated space
+MS_varmax = M * M_varimax_components;             %project original data into rotated space
 
 %normalize the microStates
 MS_pca = (MS_pca'./repmat(sqrt(diag(MS_pca'*MS_pca)),1,19))';
 MS_varmax = (MS_varmax'./repmat(sqrt(diag(MS_varmax'*MS_varmax)),1,19))';
 
 
-%% PM approach
-PM_pca_components = pca(M_stand');
+%% PM approach - temporal
+PM_pca_components = pca(M');
 MS_PM_pca = PM_pca_components(:,1:4);   %here the components are the MS
 [MS_PM_varimax,T] = rotatefactors(PM_pca_components(:,1:4));
+
+
+%% Mapping the relaxed solution to MS constrains
+
+M_pca_components = M_pca_components(:,1:4);   %pick the first four components
+
+for i = 1:size(M_pca_components,1)
+    vec = M_pca_components(i,:);
+    vec(abs(vec)<max(abs(vec))) = 0;
+    M_pca_labels(i,:) = vec/max(abs(vec));
+    
+    vec = M_varimax_components(i,:);
+    vec(abs(vec)<max(abs(vec))) = 0;
+    M_varimax_labels(i,:) = vec/max(abs(vec));
+end
+
+%% Explained variance
+M = M';
+aprox_varimax_norm = M_varimax_labels * MS_varmax';
+aprox_pca_norm = M_pca_labels * MS_pca';
+
+%measured reached residual variance
+varimax_loadings = diag(M*aprox_varimax_norm');
+pca_loadings = diag(M*aprox_pca_norm');
+aprox_varimax = aprox_varimax_norm.*repmat(varimax_loadings,1,19);
+aprox_pca = aprox_pca_norm.*repmat(pca_loadings,1,19);
+
+res_var_varimax = trace((M - aprox_varimax)*(M - aprox_varimax)')/(Nt*(Ns-1));
+res_var_pca = trace((M - aprox_pca)*(M - aprox_pca)')/(Nt*(Ns-1));
+
+%theoretical minimum residual variance
+min_res_var_varimax = trace(M*M' - (aprox_varimax_norm*M').^2)/(Nt*(Ns-1));
+min_res_var_pca = trace(M*M' - (aprox_pca_norm*M').^2)/(Nt*(Ns-1));
+
+%variance of data
+var_data = trace(M*M')/(Nt*(Ns-1));
+
+%theoretical explained variance
+theo_explained_varimax = 1 - min_res_var_varimax/var_data;
+theo_explained_pca = 1 - min_res_var_pca/var_data;
+%numerical explained variance
+explained_varimax = 1 - res_var_varimax/var_data;
+explained_pca = 1 - res_var_pca/var_data;
+
+disp(['theoretical explained variance by varimax: ',num2str(theo_explained_varimax)]);
+disp(['numerical explained variance by varimax: ',num2str(explained_varimax)]);
+disp(['theoretical explained variance by pca: ',num2str(theo_explained_pca)]);
+disp(['numerical explained variance by pca: ',num2str(explained_pca)]);
 
 %% Visualize
 figure
@@ -59,55 +105,6 @@ for i = 1:size(MS_PM_pca,2)
     subplot(2,2,i)
     topoplot(MS_PM_pca(:,i),'10-20-microst.loc','gridscale',150);
 end
-
-%% Mapping the relaxed solution to MS constrains
-
-M_pca_components = M_pca_components(:,1:4);   %pick the first four components
-
-for i = 1:size(M_pca_components,1)
-    vec = M_pca_components(i,:);
-    vec(abs(vec)<max(abs(vec))) = 0;
-    M_pca_labels(i,:) = vec/max(abs(vec));
-    
-    vec = M_varimax_components(i,:);
-    vec(abs(vec)<max(abs(vec))) = 0;
-    M_varimax_labels(i,:) = vec/max(abs(vec));
-end
-
-%% Explained variance
-M_stand = M_stand';
-aprox_varimax_norm = M_varimax_labels * MS_varmax';
-aprox_pca_norm = M_pca_labels * MS_pca';
-
-%measured reached residual variance
-varimax_loadings = diag(M_stand*aprox_varimax_norm');
-pca_loadings = diag(M_stand*aprox_pca_norm');
-aprox_varimax = aprox_varimax_norm.*repmat(varimax_loadings,1,19);
-aprox_pca = aprox_pca_norm.*repmat(pca_loadings,1,19);
-
-res_var_varimax = trace((M_stand - aprox_varimax)*(M_stand - aprox_varimax)')/(Nt*(Ns-1));
-res_var_pca = trace((M_stand - aprox_pca)*(M_stand - aprox_pca)')/(Nt*(Ns-1));
-
-%theoretical minimum residual variance
-min_res_var_varimax = trace(M_stand*M_stand' - (aprox_varimax_norm*M_stand').^2)/(Nt*(Ns-1));
-min_res_var_pca = trace(M_stand*M_stand' - (aprox_pca_norm*M_stand').^2)/(Nt*(Ns-1));
-
-%variance of data
-var_data = trace(M_stand*M_stand')/(Nt*(Ns-1));
-
-%theoretical explained variance
-theo_explained_varimax = 1 - min_res_var_varimax/var_data;
-theo_explained_pca = 1 - min_res_var_pca/var_data;
-%numerical explained variance
-explained_varimax = 1 - res_var_varimax/var_data;
-explained_pca = 1 - res_var_pca/var_data;
-
-disp(['theoretical explained variance by varimax: ',num2str(theo_explained_varimax)]);
-disp(['numerical explained variance by varimax: ',num2str(explained_varimax)]);
-disp(['theoretical explained variance by pca: ',num2str(theo_explained_pca)]);
-disp(['numerical explained variance by pca: ',num2str(explained_pca)]);
-
-
 %% Additional visualization
 
 M_pca_sq = M_pca_components.^2;   
